807f09a01f0c956093c9c4ec70a5ed8ff708f0fe
Lim jun Hwan <dlawnsghks12@naver.com>
2023-05-18 09:08:45 +0900

    230518-1(임철성)

--- final-pjt-back/finalPjtBack/accounts/models.py
+++ final-pjt-back/finalPjtBack/accounts/models.py
@@ -6,7 +6,6 @@ class User(AbstractUser):
     profile = models.ImageField(blank=True)
     background = models.ImageField(blank=True)
     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    
     # 초성 퀴즈 포인트 정보
     cho_points = models.IntegerField(default=0)
     # 줄거리 퀴즈 포인트 정보
--- final-pjt-back/finalPjtBack/accounts/serializers.py
+++ final-pjt-back/finalPjtBack/accounts/serializers.py
@@ -4,4 +4,4 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
       model=User
-      fields=('id','username','profile','background','cho_points','overview_points','followings')
+      fields=('id','username','email','profile','background','cho_points','groups','user_permissions','followings')
